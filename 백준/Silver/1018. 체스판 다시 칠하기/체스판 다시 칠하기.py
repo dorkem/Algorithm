b64f115cfc1b8c2737188d5d6b75f375cba60204
최재혁 <20233144@gs.cwnu.ac.kr>
2024-05-28 16:12:59 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,37 @@
+import sys
+
+chess1 = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+
+chess2 = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+
+n, m = map(int, sys.stdin.readline().split())
+inp = [list(sys.stdin.readline().strip()) for _ in range(n)]
+
+def ch(x, y, pattern):
+    cnt = 0
+    for i in range(8):
+        for j in range(8):
+            if pattern[i][j] == inp[x + i][y + j]:
+                cnt += 1
+    return min(cnt, 64 - cnt)
+
+res = n * m
+for i in range(n - 7):
+    for j in range(m - 7):
+        res = min(res, ch(i, j, chess1), ch(i, j, chess2))
+
+print(res)
