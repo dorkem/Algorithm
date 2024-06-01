f210a49441e2fe2aa2948c9aab97276c2e3c5b21
최재혁 <20233144@gs.cwnu.ac.kr>
2024-06-02 00:02:19 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,39 +1,8 @@
-list = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4,
-    'e': 5,
-    'f': 6,
-    'g': 7,
-    'h': 8,
-    'i': 9,
-    'j': 10,
-    'k': 11,
-    'l': 12,
-    'm': 13,
-    'n': 14,
-    'o': 15,
-    'p': 16,
-    'q': 17,
-    'r': 18,
-    's': 19,
-    't': 20,
-    'u': 21,
-    'v': 22,
-    'w': 23,
-    'x': 24,
-    'y': 25,
-    'z': 26
-}
-
 a = int(input())
-b = [i for i in input()]
-c = 0
-
-for j in range(len(b)):
-  if b[j] in list:
-    d = list[b[j]] * (31**j)
-    c += d
-
-print(c)
+word = input()
+sum = 0
+mod = 1234567891
+for i in range(len(word)):
+  b = ord(word[i]) - 96
+  sum += b * (31**i)
+print(sum % mod)
