d885bb924b1c00ad40dfbe5b9564d452dfa3745a
최재혁 <20233144@gs.cwnu.ac.kr>
2024-09-10 10:38:30 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,17 @@
+def solution(letter):
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+
+    morse_letters = letter.split(' ')
+    answer = ''
+
+    for code in morse_letters:
+        if code in morse:
+            answer += morse[code]
+    
+    return answer
