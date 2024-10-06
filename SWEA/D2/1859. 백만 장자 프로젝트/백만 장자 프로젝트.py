023b6a4dd26d79eb7b1bfee1e17cb6c4617da0c3
최재혁 <20233144@gs.cwnu.ac.kr>
2024-10-06 21:01:24 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,193 ms, Memory: 177,088 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -0,0 +1,13 @@
+T = int(input())
+for i in range(T):
+    N = int(input())
+    lst = list(map(int,input().split()))
+    answer = 0
+    max = 0
+
+    for j in lst[::-1]:
+        if j >= max:
+            max = j
+        else:
+            answer += max - j
+    print("#", i + 1, " ", answer, sep="")
