01c9b845f447c9bdd689813a1991e8647436a961
최재혁 <20233144@gs.cwnu.ac.kr>
2024-10-30 11:09:43 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 138 ms, Memory: 45,320 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,4 +1,4 @@
-class node:
+class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -9,30 +9,32 @@ def in_order(node):
         in_order(node.left)
         print(node.value, end='')
         in_order(node.right)
+
 T = 10
-for t in range(1,T+1):
+for t in range(1, T + 1):
     tc = int(input())
     nodes = {}
     arr = [list(map(str, input().split())) for _ in range(tc)]
+    
     for i in arr:
         index = int(i[0])
         value = i[1]
         if index not in nodes:
-            nodes[index] = node(value)
+            nodes[index] = Node(value)
         else:
             nodes[index].value = value
+    
+    for i in arr:
+        index = int(i[0])
         
         if len(i) > 2:
             ln = int(i[2])
-            if ln not in nodes:
-                nodes[ln] = node(arr[ln - 1][1])  # 즉시 생성 및 설정
             nodes[index].left = nodes[ln]
         
         if len(i) > 3:
             rn = int(i[3])
-            if rn not in nodes:
-                nodes[rn] = node(arr[rn - 1][1])
             nodes[index].right = nodes[rn]
+    
     print(f"#{t}", end=' ')
     in_order(nodes[1])
     print()
