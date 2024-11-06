75a48b1b9495460a504ae37be2e60f32f0cab777
최재혁 <20233144@gs.cwnu.ac.kr>
2024-11-06 11:34:13 +0900

    [D2] Title: 파스칼의 삼각형, Time: 116 ms, Memory: 45,052 KB -BaekjoonHub

---
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,15 @@
+tc = int(input())
+for i in range(1,tc+1):
+    n = int(input())
+    arr = [[0 for _ in range(n+1)] for _ in range(n+1)]
+    arr[0][1] = 1
+    print(f"#{i}")
+    for j in range(1,n):
+        for k in range(1,n+1):
+            arr[j][k] = arr[j-1][k-1] + arr[j-1][k]
+    for j in range(n):
+        word = ''
+        for k in arr[j]:
+            if k != 0:
+                word += str(k)
+        print(" ".join(word))
