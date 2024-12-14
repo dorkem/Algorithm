19088bfba7f9cd0bc12a57caa3ea072dbafcd45e
최재혁 <20233144@gs.cwnu.ac.kr>
2024-12-14 19:32:08 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,7 +1,5 @@
 def solution(array):
     cnt = 0
-    for i in range(len(array)):
-        array[i] = str(array[i])
-        words = array[i]
-        cnt += words.count('7')
+    array = str(array)
+    cnt+=array.count('7')
     return cnt
