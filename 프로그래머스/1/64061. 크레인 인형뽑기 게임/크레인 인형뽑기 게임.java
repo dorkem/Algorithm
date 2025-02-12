094c25ed0453942e7cd35d9f9e2c44b799540681
최재혁 <20233144@gs.cwnu.ac.kr>
2025-02-12 12:09:01 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.87 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,30 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        List<Integer> basket = new ArrayList<>();
-        int count = 0;
-        for (int i = 0; i < moves.length; i++) {
-            int x = moves[i];
-            for (int j = 0; j < board.length; j++) {
-                int xy = board[j][x - 1];
-                if (xy != 0) {
-                    int answer = xy;
-                    board[j][x - 1] = 0;
-
-                    if (basket.isEmpty() || basket.get(basket.size() - 1) != answer) {
-                        basket.add(xy);
-                        break;
-                        
-                    } else {
-                        basket.remove(basket.size() - 1);
-                        count++;
-                        break;
-                    }
-                }
-            }
-        }
-        return count*2;
+        int answer = 0;
+        return answer;
     }
 }
