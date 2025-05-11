390d5612b17061b4b04e0cce7e797dee4dc16c5e
최재혁 <20233144@gs.cwnu.ac.kr>
2025-05-11 11:50:16 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    result = ''
+    for i in range(len(s)):
+        if i == 0 or s[i - 1] == " ":
+            result += s[i].upper()
+        else:
+            result += s[i].lower()
+    return result
