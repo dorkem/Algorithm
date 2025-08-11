03a274482ea2315d89323dfd5a63ae8cfbfc05f3
최재혁 <20233144@gs.cwnu.ac.kr>
2025-08-11 17:20:50 +0900

    [Silver II] Title: 폴짝폴짝, Time: 148 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.java
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.java
@@ -11,30 +11,28 @@ public class Main {
         for (int i = 1; i <= N; i++) a[i] = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
 
         int[] dist = new int[N + 1];
         Arrays.fill(dist, -1);
 
         ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.add(S);
-        dist[S] = 0;
+        q.add(start);
+        dist[start] = 0;
 
         while (!q.isEmpty()) {
             int cur = q.poll();
-            if (cur == E) break;
+            if (cur == end) break;
 
             int step = a[cur];
 
-            // 앞으로 (cur + step*k)
             for (int next = cur + step; next <= N; next += step) {
                 if (dist[next] != -1) continue;
                 dist[next] = dist[cur] + 1;
                 q.add(next);
             }
 
-            // 뒤로 (cur - step*k)
             for (int next = cur - step; next >= 1; next -= step) {
                 if (dist[next] != -1) continue;
                 dist[next] = dist[cur] + 1;
@@ -42,6 +40,6 @@ public class Main {
             }
         }
 
-        System.out.println(dist[E]);
+        System.out.println(dist[end]);
     }
 }
