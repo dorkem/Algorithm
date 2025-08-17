88d5e6a63759b3031920a0afb5a7a8f2ea181402
최재혁 <20233144@gs.cwnu.ac.kr>
2025-08-17 13:51:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int[] dp = new int[12];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i<=11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++){
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+        sc.close();
+    }
+}
