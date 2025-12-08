329ebac8ddb6a7e9a09aa6fff09ad070927b82ff
최재혁 <20233144@gs.cwnu.ac.kr>
2025-12-09 08:42:20 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> helloWorld = List.of("Hello World!");
+        helloWorld.forEach(System.out::println);
+    }
+}
